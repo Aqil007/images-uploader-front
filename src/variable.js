46739ff667ed6e1a7898a2bd4fd21a374a2e9aae
Aqil <aqil6040jamadar@gmail.com>
2024-09-27 18:29:37 +0530

    done

--- src/variable.js
+++ src/variable.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:3001/api" // local
-// export const baseUrl = "https://store-image.onrender.com/api" // prod
+// export const baseUrl = "http://localhost:3001/api" // local
+export const baseUrl = "https://images-uploader-front.vercel.app/api" // prod
